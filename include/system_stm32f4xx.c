d0976bff33342b8f0667b7bc247d7a63e2d67e74
Furkan Cayci <furkanca02@gmail.com>
2020-04-29 11:54:45 +0300

    typo fix on comments

--- include/system_stm32f4xx.c
+++ include/system_stm32f4xx.c
@@ -105,9 +105,9 @@ void set_sysclk_to_168(void)
 
     /* set AHB prescaler to /1 (CFGR:bits 7:4) */
     RCC->CFGR |= (0 << 4);
-    /* set ABP low speed prescaler to /4 (APB1) (CFGR:bits 12:10) */
+    /* set APB low speed prescaler to /4 (APB1) (CFGR:bits 12:10) */
     RCC->CFGR |= (0x5 << 10);
-    /* set ABP high speed prescaler to /2 (ABP2) (CFGR:bits 15:13) */
+    /* set APB high speed prescaler to /2 (APB2) (CFGR:bits 15:13) */
     RCC->CFGR |= (0x4 << 13);
 
     /* Set M, N, P and Q PLL dividers
