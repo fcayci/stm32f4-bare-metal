66a0115a5030120f773cf1c2df7ff0c63c906aea
Furkan Cayci <furkanca02@gmail.com>
2020-05-06 13:19:02 +0300

    update itm.c to use syscall

--- projects/itm/itm.c
+++ projects/itm/itm.c
@@ -3,30 +3,40 @@
  *
  * author: Furkan Cayci
  * description:
- *    sends out a string from the swo.
- *    string can be captured using openocd
- *    config file is given within the directory
- *
+ *    SWV can be used from CubeIDE
  *
  * setup:
- *    install openocd and run
- *    openocd -f stm32f4-ocd.cfg
- *    for a few seconds. then quit with Ctrl+C
  *
- *    a new file will be generated called "swo.log"
- *    you can see the message in that file
  */
 
+#include <stdio.h>
 #include "stm32f407xx.h"
 #include "system_stm32f4xx.h"
 
-#define LEDDELAY    100000
+#define LEDDELAY    1000000
 
 /*************************************************
 * function declarations
 *************************************************/
 int main(void);
 void delay(volatile uint32_t);
+
+/* _write is a system call that uses to flush out buffers */
+// the output is directed to ITM channel 0
+// ITM_SendChar function is defined in CMSIS
+int _write(int file, char *ptr, int len){
+    int i;
+    for (i =0; i<len; i++){
+        ITM_SendChar(*ptr++);
+    }
+    return len;
+}
+
+// simply call write
+// we need other functions for interger or hex printing
+void print(char *ptr, int len){
+    _write(0, ptr, len);
+}
 
 /*************************************************
 * main code starts from here
@@ -35,47 +45,23 @@ int main(void)
 {
     set_sysclk_to_168();
 
-    // enable GPIOD clock
-    RCC->AHB1ENR |= (1 << 3);
+    char msg[] = "https://furkan.space/\n";
 
-    // make leds output
-    GPIOD->MODER &= 0x00FFFFFF;
-    GPIOD->MODER |= 0x55000000;
-
-    // clear leds
-    GPIOD->ODR = 0;
-
-    uint8_t msg[] = "http://furkan.space/\n";
-    uint8_t *pmsg = msg;
-
-    uint32_t led = 1;
     while(1)
     {
         delay(LEDDELAY);
-        GPIOD->ODR ^= (led << 12);  // Toggle LED
 
-        // Using CMSIS function directly
-        ITM_SendChar(*pmsg++);
-
-        // or manually (same thing)
-        // while (ITM->PORT[0].u8 == 0);
-        // ITM->PORT[0].u8 = *pmsg++;
-
-        // reset the message
-        if (*pmsg == '\0') {
-            pmsg = &msg[0];
-            led = (led * 2) % 15;
-        }
+        // printf is linked from libc which will have a big footprint
+        printf("%s", msg);
+        // our custom implementation of a string print function
+        print(msg, sizeof(msg));
     }
 
     return 0;
 }
 
-// A simple and not accurate delay function
-// that will change the speed based on the optimization settings
 void delay(volatile uint32_t s)
 {
     for(s; s>0; s--){
-        // Do nothing
     }
 }
