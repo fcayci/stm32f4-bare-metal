42c1e285aac00fe1699a853cffc89937f5b88a6a
Furkan Cayci <furkanca02@gmail.com>
2020-05-10 00:01:08 +0300

    update on uart tx int

--- projects/uart-tx-int/uart-tx-int.c
+++ projects/uart-tx-int/uart-tx-int.c
@@ -17,7 +17,7 @@
  *   6. calculate baud rate and set BRR
  *   7. enable uart
  *   8. setup uart handler to send out a given buffer
- *   9. enable tx interrupt from NVIC
+ *   9. enable uart interrupt from NVIC
  *   10.enable tx interrupt and disable when the buffer
  *      transmission is complete
  */
@@ -30,26 +30,22 @@
 *************************************************/
 int main(void);
 
-const uint8_t brand[] = "furkan.space\n\r";
-volatile int tx_complete = 0;
+const uint8_t msg[] = "https://furkan.space/\n\r";
 volatile int bufpos = 0;
 
 void USART2_IRQHandler(void)
 {
     // check if the source is transmit interrupt
     if (USART2->SR & (1 << 7)) {
-        // clear interrupt
-        USART2->SR &= (uint32_t)~(1 << 7);
 
-        if (bufpos == sizeof(brand)) {
+        if (bufpos == sizeof(msg)) {
             // buffer is flushed out, disable tx interrupt
-            tx_complete = 1;
-            USART2->CR1 &= (uint32_t)~(1 << 7);
+            bufpos = 0;
+            USART2->CR1 &= ~(1U << 7);
         }
         else {
             // flush ot the next char in the buffer
-            tx_complete = 0;
-            USART2->DR = brand[bufpos++];
+            USART2->DR = msg[bufpos++];
         }
     }
 }
@@ -68,27 +64,25 @@ int main(void)
     RCC->AHB1ENR |= (1 << 0);
 
     // set pin modes as alternate mode 7 (pins 2 and 3)
-    GPIOA->MODER &= 0xFFFFFF0F; // Reset bits 10-15 to clear old values
-    GPIOA->MODER |= 0x000000A0; // Set pin 2/3 to alternate func. mode (0b10)
-
-    // set pin modes as high speed
-    GPIOA->OSPEEDR |= 0x000000A0; // Set pin 2/3 to high speed mode (0b10)
+    // USART2 TX and RX pins are PA2 and PA3 respectively
+    GPIOA->MODER &= ~(0xFU << 4); // Reset bits 4:5 for PA2 and 6:7 for PA3
+    GPIOA->MODER |=  (0xAU << 4); // Set   bits 4:5 for PA2 and 6:7 for PA3 to alternate mode (10)
 
     // choose AF7 for USART2 in Alternate Function registers
-    GPIOA->AFR[0] |= (0x7 << 8); // for pin 2
-    GPIOA->AFR[0] |= (0x7 << 12); // for pin 3
+    GPIOA->AFR[0] |= (0x7 << 8); // for pin A2
+    GPIOA->AFR[0] |= (0x7 << 12); // for pin A3
 
-    // usart2 word length M, bit 12
+    // USART2 word length M, bit 12
     //USART2->CR1 |= (0 << 12); // 0 - 1,8,n
 
-    // usart2 parity control, bit 9
+    // USART2 parity control, bit 9
     //USART2->CR1 |= (0 << 9); // 0 - no parity
 
-    // usart2 tx enable, TE bit 3
+    // USART2 TX enable, TE bit 3
     USART2->CR1 |= (1 << 3);
 
-    // usart2 rx enable, RE bit 2
-    USART2->CR1 |= (1 << 2);
+    // USART2 RX enable, RE bit 2
+    //USART2->CR1 |= (1 << 2);
 
     // baud rate = fCK / (8 * (2 - OVER8) * USARTDIV)
     //   for fCK = 42 Mhz, baud = 115200, OVER8 = 0
@@ -109,20 +103,14 @@ int main(void)
     NVIC_EnableIRQ(USART2_IRQn);
 
     // now that everything is ready,
-    // enable tx interrupt and let it push out the buffer
-    tx_complete = 0;
-    bufpos = 0;
-    // enable usart2 tx interrupt
+    // enable tx interrupt and let it push out
     USART2->CR1 |= (1 << 7);
 
     while(1)
     {
-        // restart transmission
-        if (tx_complete) {
-            bufpos = 0;
-            // enable usart2 tx interrupt
-            USART2->CR1 |= (1 << 7);
-        }
+    	for(volatile int i=0; i<10000000; i++); // a long wait. should be more than enough the push out the buffer
+        // restart transmission by enabling usart2 tx interrupt
+        USART2->CR1 |= (1 << 7);
     }
 
     return 0;
