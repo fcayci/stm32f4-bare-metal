9e060dae1a431f89f27db75cb8d8eba0526055bd
Furkan Cayci <furkanca02@gmail.com>
2020-05-10 02:11:23 +0300

    Update uart.c

--- projects/uart/uart.c
+++ projects/uart/uart.c
@@ -26,6 +26,8 @@
 *************************************************/
 int main(void);
 
+volatile uint8_t msg[] = "https://furkan.space/\n";
+
 /*************************************************
 * main code starts from here
 *************************************************/
@@ -41,26 +43,27 @@ int main(void)
     RCC->AHB1ENR |= (1 << 0);
 
     // set pin modes as alternate mode 7 (pins 2 and 3)
-    GPIOA->MODER &= 0xFFFFFF0F; // Reset bits 10-15 to clear old values
-    GPIOA->MODER |= 0x000000A0; // Set pin 2/3 to alternate func. mode (0b10)
+    // USART2 TX and RX pins are PA2 and PA3 respectively
+    GPIOA->MODER &= ~(0xFU << 4); // Reset bits 4:5 for PA2 and 6:7 for PA3
+    GPIOA->MODER |=  (0xAU << 4); // Set   bits 4:5 for PA2 and 6:7 for PA3 to alternate mode (10)
 
     // set pin modes as high speed
     GPIOA->OSPEEDR |= 0x000000A0; // Set pin 2/3 to high speed mode (0b10)
 
     // choose AF7 for USART2 in Alternate Function registers
-    GPIOA->AFR[0] |= (0x7 << 8); // for pin 2
-    GPIOA->AFR[0] |= (0x7 << 12); // for pin 3
+    GPIOA->AFR[0] |= (0x7 << 8); // for pin A2
+    GPIOA->AFR[0] |= (0x7 << 12); // for pin A3
 
-    // usart2 word length M, bit 12
+    // USART2 word length M, bit 12
     //USART2->CR1 |= (0 << 12); // 0 - 1,8,n
 
-    // usart2 parity control, bit 9
+    // USART2 parity control, bit 9
     //USART2->CR1 |= (0 << 9); // 0 - no parity
 
-    // usart2 tx enable, TE bit 3
+    // USART2 TX enable, TE bit 3
     USART2->CR1 |= (1 << 3);
 
-    // usart2 rx enable, RE bit 2
+    // USART2 rx enable, RE bit 2
     USART2->CR1 |= (1 << 2);
 
     // baud rate = fCK / (8 * (2 - OVER8) * USARTDIV)
@@ -79,18 +82,17 @@ int main(void)
     // enable usart2 - UE, bit 13
     USART2->CR1 |= (1 << 13);
 
-    const uint8_t brand[] = "furkan.space\n\r";
-
     while(1)
     {
-        for (uint32_t i=0; i<sizeof(brand); i++){
+        for (uint32_t i=0; i<sizeof(msg); i++){
             // send character
-            USART2->DR = brand[i];
+            USART2->DR = msg[i];
             // wait for transmit complete
             while(!(USART2->SR & (1 << 6)));
-            // slow down
-            for(int i=0; i<1000000; i++);
+
         }
+        // slow down
+        for(int i=0; i<10000000; i++);
     }
 
     return 0;
