512cb2960d120d54be3054629071924a784028d0
Furkan Cayci <furkanca02@gmail.com>
2020-05-15 23:41:51 +0300

    blinky updates

--- projects/blinky/blinky.c
+++ projects/blinky/blinky.c
@@ -3,26 +3,23 @@
  *
  * author: Furkan Cayci
  * description:
- *    blinks 1 on-board LED at roughly 1 second intervals.
- *    system clock is running from HSI (16 Mhz)
- *    delay function is just a simple counter so is not
+ *    Blinks 1 on-board LED at roughly 1 second intervals.
+ *    system clock is running from HSI which is 16 Mhz.
+ *    Delay function is just a simple counter so is not
  *    accurate and the delay time will change based on the
  *    optimization flags.
  *
  * gpio setup steps:
+ *   There are at least three steps associated with GPIO:
  *   1. enable GPIOx clock from RCC
- *   2. set the direction of the pins from MODER
+ *   2. set the direction of the pins from MODER (input / output)
  *   3. (optional) set the speed of the pins from OSPEEDR
  *   4. (optional) set pins to pull-up or pull-down or
  *         leave them floating from PUPDR
  *   5. (optional) set output type register to push-pull or
  *         open-drain from OTYPER
  *   6. either read from IDR or write to ODR depending on
- *         input or output
- *
- * setup:
- *    uses 1 on-board LED.
- *    4 LEDs are connected to: PD12, PD13, PD14 and PD15 pins.
+ *         input or output configuration
  */
 
 #include "stm32f407xx.h"
@@ -43,8 +40,12 @@ void delay(volatile uint32_t);
 int main(void)
 {
     /* Each module is powered separately. In order to turn on a module
-     * we need to enable the relevant clock.
-     * Sine LEDs are connected to GPIOD, we need  to enable it
+     * its relevant clock needs to be enabled first.
+     * The clock enables are located at RCC module in the relevant bus registers
+     * STM32F4-Discovery board LEDs are connected to GPIOD pins 12, 13, 14, 15.
+     * All the GPIO are connected to AHB1 bus, so the relevant register for the
+     * clocks is AHB1ENR.
+     * More info in Chapter 7 - RCC in RM0090
      */
 
     /* Enable GPIOD clock (AHB1ENR: bit 3) */
@@ -57,14 +58,20 @@ int main(void)
     // Another way to write a 1 to a bit location is to shift it that much
     // Meaning shift number 1, 3 times to the left. Which would result in
     // 0b1000 or 0x8
-    // RCC->AHB1ENR |= (1 << 3);
+    // RCC->AHB1ENR |= (1U << 3);
+
+    // We can also use the predefined directives from stm header to do the same thing.
+    // RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
+    // RCC_AHB1ENR_GPIODEN here will expand to (1 << 3)
 
     /* Make Pin 12 output (MODER: bits 25:24) */
     // Each pin is represented with two bits on the MODER register
-    // 00 - input
+    // 00 - input (reset state)
     // 01 - output
     // 10 - alternate function
+    // 11 - analog mode
 
+    // We can leave the pin at default value to make it an input pin.
     // In order to make a pin output, we need to write 01 to the relevant
     // section in MODER register
     // We first need to AND it to reset them, then OR it to set them.
@@ -75,12 +82,25 @@ int main(void)
     GPIOD->MODER &= 0xFCFFFFFF;   // Reset bits 25:24 to clear old values
     GPIOD->MODER |= 0x01000000;   // Set MODER bits 25:24 to 01
 
-    // You can do the same setup with shifting
-    // GPIOD->MODER &= ~(0x3 << 24)  or GPIOD->MODER &= ~(0b11 << 24);
-    // GPIOD->MODER |=  (0x1 << 24)  or GPIOD->MODER |=  (0b01 << 24);
+    // We could also use a more systematic approach by shifting to bits
+    // GPIOD->MODER &= ~(3U << 24);
+    // GPIOD->MODER |=  (1U << 24);
+
+    // Since each pin is represented with two bits, we needed to shift 24 times
+    // to write to the bits related to pin 12 (times 2). We could change our code to
+    // auto-calculate the relevant bit location.
+    // GPIOD->MODER &= ~(3U << 2*12);
+    // GPIOD->MODER |=  (1U << 2*12);
+
+    // As a side note:
+    // First AND operations to clear the bits is not really important for first
+    // boot up setup, but if we ever want to change the pin behavior in the middle
+    // of the code, we need to make sure it is cleared before writing it.
 
     /* We do not need to change the speed of the pins, leave them floating
      * and leave them as push-pull, so no need to touch OTYPER, OSPEEDR, and OPUPDR
+     * However, if we want to use a open-drain required protocol (i.e. I2C), we need
+     * to declare the pin as open-drain from OTYPER register
      */
 
     /* Set or clear pins (ODR: bit 12) */
@@ -88,13 +108,14 @@ int main(void)
     // ODR: xxx1 XXXX XXXX XXXX
     GPIOD->ODR |= 0x1000;
 
-    // You can do the same with shifting
-    // GPIOD->ODR |= (1 << 12);
+    // With shifting
+    // GPIOD->ODR |= (1U << 12);
 
+    // the code should never leave its master loop, hence while(1) or for(;;)
     while(1)
     {
         delay(LEDDELAY);
-        GPIOD->ODR ^= (1 << 12);  // Toggle LED
+        GPIOD->ODR ^= (1U << 12);  // Toggle LED
     }
 
     __asm("NOP"); // Assembly inline can be used if needed
@@ -105,7 +126,5 @@ int main(void)
 // that will change the speed based on the optimization settings
 void delay(volatile uint32_t s)
 {
-    for(s; s>0; s--){
-        // Do nothing
-    }
+    for(; s>0; s--);
 }
